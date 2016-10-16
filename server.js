deebb1865dff98a01acb439e3c1262d3a1c14e0a
Victor Tran <murtato@gmail.com>
2016-10-15 20:49:52 -0700

    troubleshooting todos

--- server.js
+++ server.js
@@ -9,9 +9,14 @@ var app = express();
 // configure the app (app.set)
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+app.locals.title = 'First Express';
 
 // mount middleware (app.use)
-app.user(bodyParser.urlencoded({extended: true})); //allows express to parse data in html forms
+app.use(function(req, res, next) {
+  console.log(req.headers['user-agent']);
+  next();
+});
+app.use(express.static(path.join(__dirname, 'public')));
 
 // require and mount (app.use) routes
 
@@ -25,7 +30,8 @@ app.get('/goodbye', function(req, res) {
   res.json( {msg: 'Goodbye World'} );
 });
 app.get('/todos', function(req, res) {
-  var todos = [
+  app.locals.title = 'First Express';
+  app.locals.todos = [
     {todo: 'Feed dogs', done: true},
     {todo: 'Learn Express', done: false},
     {todo: 'Have fun', done: true}
@@ -33,6 +39,14 @@ app.get('/todos', function(req, res) {
   res.render('todos/index', {
     todos: todos
   });
+});
+app.post('/todos', function(req, res){
+  app.locals.todos.push({
+    todo: req.body.newTodo,
+    done: false
+  });
+  console.log(req.body.newTodo);
+  res.render('todos/index');
 });
 
 // Tell the app to listen on port 3000
