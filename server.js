22c956a4853dc795ca2966d414ecd91509d8c86c
Victor Tran <murtato@gmail.com>
2016-10-16 17:29:43 -0700

    basic routing architecture on server.js  complete

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
 // Load express and rqeuire modules
 var express = require('express');
 var path = require ('path');
-var bodyParser = require('body-parser')
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 
+// var routes = require('./routes/index');
 // Create our express app
 var app = express();
 
 // configure the app (app.set)
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
-app.locals.title = 'First Express';
+app.locals.title = 'Lease BREaker';
 
 // mount middleware (app.use)
 app.use(function(req, res, next) {
@@ -17,9 +21,10 @@ app.use(function(req, res, next) {
   next();
 });
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 
 // require and mount (app.use) routes
-
 
 // Define a root route directly on app
 // Later, we will use the router object
@@ -30,7 +35,7 @@ app.get('/goodbye', function(req, res) {
   res.json( {msg: 'Goodbye World'} );
 });
 app.get('/todos', function(req, res) {
-  app.locals.title = 'First Express';
+  app.locals.title = 'Lease BREaker';
   app.locals.todos = [
     {todo: 'Feed dogs', done: true},
     {todo: 'Learn Express', done: false},
@@ -40,6 +45,7 @@ app.get('/todos', function(req, res) {
     todos: todos
   });
 });
+app.locals.todos = require('./data/todos');
 app.post('/todos', function(req, res){
   app.locals.todos.push({
     todo: req.body.newTodo,
