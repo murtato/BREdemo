a39da0a763714cce5693e8bf5cbe2a28c90567a2
Victor Tran <murtato@gmail.com>
2016-10-18 11:16:12 -0700

    added express sessions module and angular 2

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 // Load express and rqeuire modules
-var express = require('express');
-var path = require ('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var Materialize = require('node-materialize');
-var mongoose = require('mongoose');
+var express         = require('express');
+var path            = require ('path');
+var favicon         = require('serve-favicon');
+var logger          = require('morgan');
+var cookieParser    = require('cookie-parser');
+var bodyParser      = require('body-parser');
+var Materialize     = require('node-materialize');
+var mongoose        = require('mongoose');
+var session         = require('express-session');
 // var routes = require('./routes/index');
 // var users = require('./routes/users');
 
